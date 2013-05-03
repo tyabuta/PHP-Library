bf86eec9ed27c650a283cc9ff964382e34af6762
tyabuta <gmforip@gmail.com>
2013-05-03 22:06:14 +0900

    modified:   macro.php
    リクエストパラメータからBool値を取得する関数を追加
    SQLインジェクション対策の関数名変更

--- macro.php
+++ macro.php
@@ -44,33 +44,136 @@ function HTTPPost($url, $params){
 }
 
 
+
+
+
 /*
- * SQLインジェクションを防止する為、無効な文字列を排除する。
- * プリペアドステートメントを使う場合は必要なし。
- */
-function SQLSanitizeForInt($val){
-    return intval($val);
+* 赤いボタン用のCSSを出力する。
+*/
+function CSSRenderRedButton(){
+    print <<<"EOF"
+.RedButton {
+    padding: 0.5em;
+    display: inline-block;
+    font-size: 14px;
+    color: #fff;
+    font-weight: bold;
+    letter-spacing: 0.1em;
+    background-image: -webkit-linear-gradient(top, #be0000, #990000);
+    background-image: -moz-linear-gradient(top, #be0000, #990000);
+    background-image: -o-linear-gradient(top, #be0000, #990000);
+    background-image: -linear-gradient(to bottom, #be0000, #990000);
+    border-radius: 10px;
+    box-shadow: 0px 5px 2px #333, 0px 0px 3px #e5acac inset;
+    text-shadow: 1px 1px 1px rgba(0,0,0,0.3);
+    border: 2px solid #990000;
+}
+EOF;
+}
+
+
+
+/* ------------------------------------------------------------------
+* リクエスト解析
+-------------------------------------------------------------------*/
+
+// string valreq(string $q, mixed $def="")
+// string valpos(string $q, mixed $def="")
+// string valget(string $q, mixed $def="")
+// string boolRequestWithParamName(string $q, bool $def=false)
+// string paramWithCmdLine(string $q, int $i, mixed $def="")
+
+
+/*
+* $_REQUESTに値が設定されていない場合、デフォルト値を使用する。
+*
+*   $q: リクエストインデックス
+* $def: デフォルト値(省略時は空文字列)
+*/
+function valreq($q, $def=""){
+    if (!isset($_REQUEST[$q]) || empty($_REQUEST[$q])) return $def;
+    return $_REQUEST[$q];
+}
+
+
+/*
+* $_POSTに値が設定されていない場合、デフォルト値を使用する。
+*
+*   $q: リクエストインデックス
+* $def: デフォルト値(省略時は空文字列)
+*/
+function valpos($q, $def=""){
+    if (!isset($_POST[$q]) || empty($_POST[$q])) return $def;
+    return $_POST[$q];
 }
 
 /*
- * SQLインジェクションを防止する為、無効な文字列を排除する。
- * プリペアドステートメントを使う場合は必要なし。
- */
-function SQLSanitizeForDouble($val){
-    return doubleval($val);
+* $_GETに値が設定されていない場合、デフォルト値を使用する。
+*
+*   $q: リクエストインデックス
+* $def: デフォルト値(省略時は空文字列)
+*/
+function valget($q, $def=""){
+    if (!isset($_GET[$q]) || empty($_GET[$q])) return $def;
+    return $_GET[$q];
 }
+
 
 /*
- * SQLインジェクションを防止する為、特殊文字をエスケープする。
- * プリペアドステートメントを使う場合は必要なし。
- */
-function SQLSanitizeForString($val){
-    return addslashes($val);
+* $_REQUEST からBool型の値を取得する。
+* 値が設定されていない場合、デフォルト値を使用する。
+* yes/no y/n true/false 1/0の指定が可能
+*
+*   $q: リクエストインデックス
+* $def: デフォルト値(省略時はfalse)
+*/
+function boolRequestWithParamName($q, $def=false){
+    if (!empty($_REQUEST)){
+        if (isset($_REQUEST[$q]) && !empty($_REQUEST[$q])){
+            $val = $_REQUEST[$q];
+            if (preg_match('/^(y|yes|true)$/i', $val)){
+                return true;
+            }
+            if (preg_match('/^(n|no|false)$/i', $val)){
+                return false;
+            }
+            
+            return (0 != $val);        
+        }
+    }
+    return $def;
+}
+
+
+/*
+* $_REQUESTに値が設定されていない場合、
+* コマンドライン引数から値を取得する。
+* どちらも値が設定されていないなら、$defを使用する。
+*
+*   $q: リクエストインデックス
+*   $i: コマンドライン引数インデックス
+* $def: デフォルト値(省略時は空文字列)
+*
+*/
+function paramWithCmdLine($q, $i, $def=""){
+    if (!empty($_REQUEST[$q])){
+        return $_REQUEST[$q];
+    }
+
+    global $argv;
+    if ($argv[$i]){
+        return $argv[$i];
+    }
+   
+    return $def;    
 }
 
 
 
 
+/* ------------------------------------------------------------------
+* Scriptインジェクション
+-------------------------------------------------------------------*/
 
 /*
 * HTML出力用のサニタイズ関数
@@ -79,6 +182,47 @@ function SQLSanitizeForString($val){
 function HTMLSanitize($val){
     return htmlspecialchars($val, ENT_QUOTES);
 }
+
+/* ------------------------------------------------------------------
+* SQLインジェクション
+-------------------------------------------------------------------*/
+
+/*
+ * SQLインジェクションを防止する為、無効な文字列を排除する。
+ * プリペアドステートメントを使う場合は必要なし。
+ */
+function SQLSanitizeWithInt($val){
+    return intval($val);
+}
+
+/*
+ * SQLインジェクションを防止する為、無効な文字列を排除する。
+ * プリペアドステートメントを使う場合は必要なし。
+ */
+function SQLSanitizeWithDouble($val){
+    return doubleval($val);
+}
+
+/*
+ * SQLインジェクションを防止する為、特殊文字をエスケープする。
+ * プリペアドステートメントを使う場合は必要なし。
+ */
+function SQLSanitizeWithString($val){
+    return addslashes($val);
+}
+
+
+
+
+/* ------------------------------------------------------------------
+* HTMLRender
+-------------------------------------------------------------------*/
+// void HTMLRenderBadRequest()
+// void HTMLRenderForbidden()
+// void HTMLRenderMeta()
+// void HTMLRenderHtml5js()
+// void HTMLRenderjQuery()
+
 
 /*
 * 400 Bad Request 用の出力関数
@@ -134,91 +278,4 @@ function HTMLRenderjQuery(){
 <script src="http://code.jquery.com/mobile/1.3.1/jquery.mobile-1.3.1.min.js"></script>
 EOF;
 }
-
-
-/*
-* 赤いボタン用のCSSを出力する。
-*/
-function CSSRenderRedButton(){
-    print <<<"EOF"
-.RedButton {
-    padding: 0.5em;
-    display: inline-block;
-    font-size: 14px;
-    color: #fff;
-    font-weight: bold;
-    letter-spacing: 0.1em;
-    background-image: -webkit-linear-gradient(top, #be0000, #990000);
-    background-image: -moz-linear-gradient(top, #be0000, #990000);
-    background-image: -o-linear-gradient(top, #be0000, #990000);
-    background-image: -linear-gradient(to bottom, #be0000, #990000);
-    border-radius: 10px;
-    box-shadow: 0px 5px 2px #333, 0px 0px 3px #e5acac inset;
-    text-shadow: 1px 1px 1px rgba(0,0,0,0.3);
-    border: 2px solid #990000;
-}
-EOF;
-}
-
-/*
-* $_REQUESTに値が設定されていない場合、
-* コマンドライン引数から値を取得する。
-* どちらも値が設定されていないなら、$defを使用する。
-*
-*   $q: リクエストインデックス
-*   $i: コマンドライン引数インデックス
-* $def: デフォルト値(省略時は空文字列)
-*
-*/
-function paramWithCmdLine($q, $i, $def=""){
-    if (!empty($_REQUEST[$q])){
-        return $_REQUEST[$q];
-    }
-
-    global $argv;
-    if ($argv[$i]){
-        return $argv[$i];
-    }
-   
-    return $def;    
-}
-
-
-
-/*
-* $_REQUESTに値が設定されていない場合、デフォルト値を使用する。
-*
-*   $q: リクエストインデックス
-* $def: デフォルト値(省略時は空文字列)
-*/
-function valreq($q, $def=""){
-    if (!isset($_REQUEST[$q]) || empty($_REQUEST[$q])) return $def;
-    return $_REQUEST[$q];
-}
-
-
-/*
-* $_POSTに値が設定されていない場合、デフォルト値を使用する。
-*
-*   $q: リクエストインデックス
-* $def: デフォルト値(省略時は空文字列)
-*/
-function valpos($q, $def=""){
-    if (!isset($_POST[$q]) || empty($_POST[$q])) return $def;
-    return $_POST[$q];
-}
-
-/*
-* $_GETに値が設定されていない場合、デフォルト値を使用する。
-*
-*   $q: リクエストインデックス
-* $def: デフォルト値(省略時は空文字列)
-*/
-function valget($q, $def=""){
-    if (!isset($_GET[$q]) || empty($_GET[$q])) return $def;
-    return $_GET[$q];
-}
-
-
-
 
